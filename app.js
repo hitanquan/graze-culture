5e3edcc348ae97da96ecd9278ae67ba4c6024d8f
muyou <2685303161@qq.com>
2024-11-08 16:37:47 +0800

    update 删除没用的云开发代码

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 // app.js
 App({
   onLaunch() {
-    // 云开发初始化
-    // 初始化完成之后，可以使用云能力，例如调用云函数等
-    wx.cloud.init({
-      // 云开发环境的配置信息
-      env: 'cloud-dev-server-3frnvwnfaf34ffa', // 云环境ID
-      traceUser: true,
-    })
-
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
